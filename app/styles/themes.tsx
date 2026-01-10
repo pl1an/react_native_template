52c259750399dd3dd293c720dd3a01838b51440d
ian <paletaian56@gmail.com>
2026-01-10 04:14:50 -0300

    feat: added theme template

---
+++ app/styles/themes.tsx
@@ -0,0 +1,11 @@
+import { Theme } from "../types/themes";
+
+export const themes: { [key: string]: Theme } = {
+    default: {
+        background:"#1f1f1f",
+        primary:"white",
+        secondary:"grey",
+        tagbackground:"grey",
+        secondary_screen_shadow: "rgba(1, 1, 1, 0.5)"
+    },
+}
---
+++ app/types/themes.tsx
@@ -0,0 +1,7 @@
+export interface Theme {
+    background: string;
+    primary: string; 
+    secondary: string;
+    tagbackground: string;
+    secondary_screen_shadow: string;
+}
